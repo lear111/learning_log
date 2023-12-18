1d9c043df3959a0b79a5053acaf48a0c597c77d2
lear111 <wangguosongwgs@126.com>
2023-12-18 20:32:24 +0800

    9

--- learning_log/manage.py
+++ learning_log/manage.py
@@ -1,23 +1,10 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
-    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
